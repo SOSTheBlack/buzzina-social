a600dd087573ad7bb56b23c627ab0411ef335518
Jean C. Garcia <jeancesargarcia@gmail.com>
2024-11-24 10:56:09 -0300

    chore: configurações

--- config/variables.php
+++ config/variables.php
@@ -1,11 +1,11 @@
 <?php
 // Variables
 return [
-  "creatorName" => "Pixinvent",
-  "creatorUrl" => "https://pixinvent.com",
-  "templateName" => "Materialize",
-  "templateSuffix" => "Materialize - Bootstrap 5 HTML Laravel Admin Template",
-  "templateVersion" => "2.0.0",
+  "creatorName" => "Garcia Softwares",
+  "creatorUrl" => "https://buzzinasocial.com.br",
+  "templateName" => "Buzzina Social",
+  "templateSuffix" => "Buzzina Social - Fazendo barulho nas redes sociais",
+  "templateVersion" => "v0.0.1",
   "templateFree" => false,
   "templateDescription" => "Materialize – is the most developer friendly & highly customizable Admin Dashboard Template.",
   "templateKeyword" => "dashboard, material, material design, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5",
--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,9 @@ use App\Http\Controllers\authentications\RegisterBasic;
 Route::get('/', [HomePage::class, 'index'])->name('pages-home');
 Route::get('/page-2', [Page2::class, 'index'])->name('pages-page-2');
 
-// locale
-Route::get('/lang/{locale}', [LanguageController::class, 'swap']);
 Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
 
-// authentication
-Route::get('/auth/login-basic', [LoginBasic::class, 'index'])->name('auth-login-basic');
-Route::get('/auth/register-basic', [RegisterBasic::class, 'index'])->name('auth-register-basic');
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('/');
     })->name('dashboard');
